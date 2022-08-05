f698188a9d28754dfae8b78e4968533b2ced260f
Saim Nasır <saim.nasir.65@gmail.com>
2022-08-06 00:08:38 +0300

    Organization editor ui changed

--- src/client/src/app/_model/organization.model.ts
+++ src/client/src/app/_model/organization.model.ts
@@ -1,4 +1,5 @@
 import { BaseModel } from "src/app/_model/base-model";
+import { OrganizationTypeModel } from "./organization-type.model";
 
 export class OrganizationModel implements BaseModel <string>  {
      created: Date;
@@ -7,5 +8,10 @@ export class OrganizationModel implements BaseModel <string>  {
      title: string; 
      id: string; 
      active:boolean = true;
+
      organizationTypeId: string; 
+     organizationType: OrganizationTypeModel; 
+     
+     parentOrganizationId: string; 
+     parentOrganization:OrganizationModel; 
 }
--- src/client/src/app/memory/organization/organization-editor/organization-editor.component.ts
+++ src/client/src/app/memory/organization/organization-editor/organization-editor.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrganizationTypeModel } from 'src/app/_model/organization-type.model';
 import { OrganizationModel } from 'src/app/_model/organization.model';
 import { OrganizationTypeService } from 'src/app/_service/organization-type.service';
 import { OrganizationService } from 'src/app/_service/organization.service';
+import { Option } from 'src/app/_model/option.model';
 
 @Component({
   selector: 'app-organization-editor',
@@ -12,44 +14,101 @@ import { OrganizationService } from 'src/app/_service/organization.service';
 })
 export class OrganizationEditorComponent implements OnInit {
 
-  model = new OrganizationModel();
-  organizationTypes: Array<OrganizationTypeModel>;
-  isNew = false;
-  title :string;
-  constructor(private organizationService: OrganizationService,
-    private organizationTypeService: OrganizationTypeService, 
+  form: FormGroup;
+
+  organizationTypes: Array<Option>;
+  parentOrganizations: Array<Option>;
+  title: string;
+  action: string = 'add';
+  id: string | null;
+  get isRead() {
+    return this.action === 'read'
+  }
+  errorMessage: string | null;
+  showError: boolean = false;
+  constructor(private formBuilder: FormBuilder,
+    private organizationService: OrganizationService,
+    private organizationTypeService: OrganizationTypeService,
     private route: ActivatedRoute,
     protected router: Router) { }
 
 
   ngOnInit(): void {
 
-    this.loadOrganizationTypes();
+    this.id = this.route.snapshot.paramMap.get('id');
+
     this.getModel();
+    this.loadOrganizations();
+    this.loadOrganizationTypes();
+    this.initForm();
   }
 
-  loadOrganizationTypes() {  
-      this.organizationTypeService.getAll(this.organizationTypeService.route).subscribe({
-        next: (response) => {
-          if (response.success) {
-           this.organizationTypes = response.data.data; 
-           this.title = this.model.title;
+  private initForm() {
+    this.form = this.formBuilder.group({
+      id: new FormControl(null),
+      title: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(100),
+      ]),
+
+      organizationTypeId: new FormControl(null, [
+        Validators.required,
+      ]),
+
+      parentOrganizationId: new FormControl(null, [        
+      ]),
+      active: new FormControl(true)
+    },
+    );
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.form.controls;
+  }
+
+  loadOrganizations() {
+    this.parentOrganizations = [];
+    this.organizationService.getAll(this.organizationService.route).subscribe({
+      next: (response) => {
+        if (response.success) {
+          let organizationList = response.data.data;
+          // prevent self parenting and circular parenting
+          if (this.id) {
+            let organization = organizationList.find(s => s.id == this.id); 
+            organizationList = organizationList.filter(s => s.id != organization?.id && s.id != organization?.parentOrganizationId && s.parentOrganizationId != organization?.id);          
           }
-        },
-        error: (e) => console.error(e),
-        complete: () => console.info('complete')
-      }); 
+          this.parentOrganizations = organizationList.map(({ id, title }) => (new Option(id, title)));
+        }
+      },
+      error: (e) => console.error(e),
+      //complete: () => console.info('complete')
+    });
+  }
+
+
+  loadOrganizationTypes() {
+    this.organizationTypeService.getAll(this.organizationTypeService.route).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.organizationTypes = response.data.data.map(({ id, title }) => (new Option(id, title)));
+        }
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
   }
 
   getModel() {
     let id = this.route.snapshot.paramMap.get('id');
-    this.isNew = id == null;
+    this.action = this.route.snapshot.url[1].toString();
     if (id) {
       this.organizationService.read(this.organizationService.route, id).subscribe({
         next: (response) => {
           if (response.success) {
-            this.model = response.data.data;
-            
+            this.form.patchValue(response.data.data);
+            this.title = this.form.value.title;
           }
         },
         error: (e) => console.error(e),
@@ -59,8 +118,17 @@ export class OrganizationEditorComponent implements OnInit {
   }
 
   save() {
-    if (this.isNew) {
-      this.organizationService.create(this.organizationService.route, this.model).subscribe({
+
+    this.showError = !this.form.valid;
+    console.log('form', this.form.value);
+
+    this.errorMessage = null;
+    if (!this.form.valid) {
+      this.errorMessage = 'Please check invalid fields!'
+      return;
+    }
+    if (this.action === 'add') {
+      this.organizationService.create(this.organizationService.route, this.form.value).subscribe({
         next: (response) => {
           if (response.success) {
             console.info('created');
@@ -71,7 +139,7 @@ export class OrganizationEditorComponent implements OnInit {
         complete: () => console.info('complete')
       });
     } else {
-      this.organizationService.update(this.organizationService.route, this.model).subscribe({
+      this.organizationService.update(this.organizationService.route, this.form.value).subscribe({
         next: (response) => {
           if (response.success) {
             console.info('updated');
