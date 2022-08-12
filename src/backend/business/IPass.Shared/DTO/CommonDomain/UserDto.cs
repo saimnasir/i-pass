64f7c11f57e32e2ebd194103f53e156775a03c2b
Saim Nasır <saim.nasir.65@gmail.com>
2022-08-12 09:13:22 +0300

    Adding email field to user dto

--- src/backend/business/IPass.Shared/DTO/CommonDomain/UserDto.cs
+++ src/backend/business/IPass.Shared/DTO/CommonDomain/UserDto.cs
@@ -13,6 +13,7 @@ namespace IPass.Shared.DTO.CommonDomain
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+        public string Email { get; set; }
         public DateTime? BirthDate { get; set; }
         public string PhotoId { get; set; } = string.Empty; 
         public DateTime LastSeen { get; set; }
