e0a78e391c7f71bbc672d456f3ae2cc12cdcd1bf
Saim Nasır <saim.nasir.65@gmail.com>
2022-08-12 09:14:05 +0300

    profile ui completed

---
+++ src/client/src/app/account/my-profile/my-profile.component.ts
@@ -0,0 +1,52 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AccountService } from '../../_service/account.service';
+import { AlertService } from '../../_service/alert.service';
+import { ProfileModel } from '../../_model/user.model';
+import { PinCodeService } from 'src/app/_service/pin-code.service';
+import { PinCodeModel } from 'src/app/_model/pin-code.model';
+
+
+@Component({
+    selector: 'app-my-profile',
+    templateUrl: './my-profile.component.html',
+    styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent implements OnInit {
+    model = new ProfileModel();   
+    loading = false;
+    returnUrl: string;
+    errorMessage: string | undefined;
+    constructor(
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {  
+        this.getModel();
+    }
+  
+    get user(){
+        return this.model?.user;
+    }
+
+    getModel() {
+        this.loading = true;
+        this.accountService.getProfile().subscribe({
+            next: (response) => {
+                if (response.success) {
+                    this.model = response.data.data;
+                    if (!this.model.pinCode) {
+                        this.model.pinCode = new PinCodeModel();
+                    }
+                }
+            },
+            error: (e) => console.error(e),
+            complete: () => {
+                this.loading = false;
+                console.info('complete');
+            }
+        });
+    }
+
+}
--- src/client/src/app/account/settings/settings.component.ts
+++ src/client/src/app/account/settings/settings.component.ts
@@ -1,38 +1,38 @@
 ﻿import { Component, Input, OnInit } from '@angular/core';
 import { AccountService } from '../../_service/account.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { PhoneNumberValidationMessages } from 'src/app/_static-data/consts';
-import { User } from 'src/app/_model/user.model';
+import { PinCodeValidationMessages } from 'src/app/_static-data/consts';
+import { PinCodeService } from 'src/app/_service/pin-code.service';
+import { PinCodeModel } from 'src/app/_model/pin-code.model';
 
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.css']
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class SettingsComponent implements OnInit {
     form: FormGroup; 
     loading = false;
     submitted = false;
     returnUrl: string;
-
     errorMessage: string | null;
     showError: boolean = false;
     phonePattern: string | RegExp = new RegExp('^[0-9]{10}$');
     editable = false;
-
-    @Input() user : User;
+    
+    @Input() pinCode : PinCodeModel;
     constructor(private formBuilder: FormBuilder, 
         private accountService: AccountService, 
+        private pinCodeService: PinCodeService, 
     ) { }
 
-    get phoneNumberValidationMessages() {
-        return PhoneNumberValidationMessages;
+    get pinCodeValidationMessages() {
+        return PinCodeValidationMessages;
     }
     
     ngOnInit() {
-        this.initForm();
-        this.form.patchValue(this.user); 
+        this.initForm(); 
     }
 
     // convenience getter for easy access to form fields
@@ -44,32 +44,29 @@ export class ProfileComponent implements OnInit {
         this.form = this.formBuilder.group
             ({
                 id: new FormControl(null),
-                phoneNumber: new FormControl(null, [
+                code: new FormControl(null, [
                     Validators.required,
-                    Validators.minLength(10),
-                    Validators.maxLength(10),
+                    Validators.minLength(4),
+                    Validators.maxLength(4),
                 ]),
-                email: new FormControl(null),
-                firstName: new FormControl(null, [
-                    Validators.required, 
-                ]),
-                lastName: new FormControl(null, [
-                    Validators.required, 
-                ]),
+                active : new FormControl(true),
             });
+            this.form.patchValue(this.pinCode);        
     }
     
-    saveProfile() {
-        this.accountService.updateProfile(this.form.value).subscribe({
+    saveSettings() {
+        this.pinCodeService.create(this.pinCodeService.route, this.form.value).subscribe({
             next: (response) => {
                 if (response.success) {
-                    console.info('updated');
-                    
+                    console.info('created');
+                    this.editable = false;
                 }
-                this.editable = false;
             },
             error: (e) => console.error(e),
             complete: () => console.info('complete')
         });
-    } 
+    }
+
+ 
+   
 }
